9e96fc46c629604cf00958dd25eafdee03986baa
Shivani112s <sollushivani@gmail.com>
2025-06-11 14:10:03 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,13 @@
+let display = document.getElementById("display");
+
+function press(val) {
+  display.value += val;
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
+}
